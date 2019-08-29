c67811283e0921a5bffa096231b3d40b4b5c3982
abdulRehman06 <arkaimkhani06@gmail.com>
2019-08-29 22:31:39 +0100

    Updates

---
+++ precache-manifest.031e160b10e394470e3cd8a4171b2f4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcb7251cdbb463c0f6efb0c074cf68af",
+    "url": "/tic-tac-toe-game/index.html"
+  },
+  {
+    "revision": "291c35ed65f1e49a951a",
+    "url": "/tic-tac-toe-game/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "0aa157e31e9588507b48",
+    "url": "/tic-tac-toe-game/static/js/2.c2c2e04a.chunk.js"
+  },
+  {
+    "revision": "291c35ed65f1e49a951a",
+    "url": "/tic-tac-toe-game/static/js/main.c2d8fc2b.chunk.js"
+  },
+  {
+    "revision": "0fc3b3690a6415c0dca1",
+    "url": "/tic-tac-toe-game/static/js/runtime~main.55e20e0a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tic-tac-toe-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
